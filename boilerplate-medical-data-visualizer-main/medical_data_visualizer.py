5cdf285fd079f5b37ea2d7d55bee29eee1f6ff89
bruscons80 <bruno.martins@sou.inteli.edu.br>
2025-09-07 16:51:07 -0300

    Answer
    Manda a resposta para  a main

--- boilerplate-medical-data-visualizer-main/medical_data_visualizer.py
+++ boilerplate-medical-data-visualizer-main/medical_data_visualizer.py
@@ -3,58 +3,50 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
 
-# 1
-df = None
+# 1. Importaçã0 dos dados de medical_examination.csv 
+df = pd.read_csv('medical_examination.csv')
 
-# 2
-df['overweight'] = None
+# 2. Adiciona a coluna overweight
+df['overweight'] = (df['weight'] / (df['height'] / 100) ** 2).apply(lambda x: 1 if x > 25 else 0)
 
-# 3
+# 3. Normaliza os dados com 0 e 1
+df['cholesterol'] = df['cholesterol'].apply(lambda x: 0 if x == 1 else 1)
+df['gluc'] = df['gluc'].apply(lambda x: 0 if x == 1 else 1)
 
-
-# 4
+# 4. Desenha plot categorico em draw_cat_plot function.
 def draw_cat_plot():
-    # 5
-    df_cat = None
+    # 5. Cria o df
+    df_cat = pd.melt(df, id_vars=['cardio'], value_vars=['cholesterol', 'gluc', 'smoke', 'alco', 'active', 'overweight'])
 
+    # 6. Reformata e agrupa os dados do df
+    df_cat = df_cat.groupby(['cardio', 'variable', 'value']).size().reset_index(name='total')
 
-    # 6
-    df_cat = None
-    
+    # 7. Converte os dados em log e cria um grafico
+    fig = sns.catplot(x='variable', y='total', hue='value', col='cardio', data=df_cat, kind='bar').fig
 
-    # 7
-
-
-
-    # 8
-    fig = None
-
-
-    # 9
     fig.savefig('catplot.png')
     return fig
 
-
-# 10
+# 10. Desenha um "Mapa de Calor"
 def draw_heat_map():
-    # 11
-    df_heat = None
+    # 11. Limpa os dados
+    df_heat = df[
+        (df['ap_lo'] <= df['ap_hi']) &
+        (df['height'] >= df['height'].quantile(0.025)) &
+        (df['height'] <= df['height'].quantile(0.975)) &
+        (df['weight'] >= df['weight'].quantile(0.025)) &
+        (df['weight'] <= df['weight'].quantile(0.975))
+    ]
 
-    # 12
-    corr = None
+    # 12. Calcula a matrix de coorelação
+    corr = df_heat.corr()
+    mask = np.triu(np.ones_like(corr, dtype=bool))
 
-    # 13
-    mask = None
+    fig, ax = plt.subplots(figsize=(12, 12))
 
+    # 15. Plota a correlação
+    sns.heatmap(corr, annot=True, fmt='.1f', mask=mask, vmax=.3, center=0,
+                square=True, linewidths=.5, cbar_kws={"shrink": .5})
 
-
-    # 14
-    fig, ax = None
-
-    # 15
-
-
-
-    # 16
     fig.savefig('heatmap.png')
     return fig
